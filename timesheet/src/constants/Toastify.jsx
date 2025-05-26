1de3278491b5211c625504f8bcd8bd42f87c5343
Naveen-aero <naveen.a@aer0360.co.in>
2025-05-26 14:08:58 +0530

    Toastify

---
+++ timesheet/src/constants/Toastify.jsx
@@ -0,0 +1,65 @@
+// src/utils/toast.js
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Success notification
+export const showSuccessToast = (message) => {
+  toast.success(message, {
+    position: "top-center",
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    style: { backgroundColor: "white", color: "#82B97E" }, // Inline styles for red background
+  });
+};
+
+// Error notification
+export const showErrorToast = (message) => {
+  toast.error(message, {
+    position: "top-center",
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    style: { backgroundColor: "white", color: "#FF2400" },
+  });
+};
+
+// Info notification
+export const showInfoToast = (message) => {
+  toast.info(message, {
+    position: "top-center",
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+};
+
+// Warning notification
+export const showWarningToast = (message) => {
+  toast.warn(message, {
+    position: "top-center",
+    autoClose: 1500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    style: { backgroundColor: "white", color: "#FCD12A" },
+  });
+};
+
+// Toast container (to be added in your main App component)
+export const ToastContainerComponent = () => <ToastContainer />;
